c6e3ea93d289ea91c988539ea381bc27e926fa9d
cwahlfeldt <chriswahlfeldt@gmail.com>
2018-07-13 13:39:50 -0500

    some styleing

--- src/index.js
+++ src/index.js
@@ -1,39 +1,51 @@
 import { h, app } from 'hyperapp'
 import { queuers } from './mockdata.yml'
 import Nav from './components/Nav'
+import moment from 'moment'
 
 import './style.css'
 import 'tachyons-custom'
+import './vars.css'
+
+const time = moment().toISOString();
 
 const data = queuers
 
 const state = {
   queuers: null,
+  waitingTimes: [],
 }
 
 const actions = {
-  setQueuers: state => ({queuers: data})
+  setQueuers: state => ({queuers: data}),
+  startTimers: (time, index) => (state, actions) => {
+    setInterval(actions.setTime(time, index), 1000)
+  },
+  setTime
 }
 
 const renderQueue = (queuers) => (
   queuers.map((queuer, index) => (
-    <div class="flex flex-row">
-      <h4 class="name avenir black fw5 f4">{index + 1}</h4>
-      <h4 class="name avenir black fw3 f4">{queuer.name}</h4>
-      <h4 class="name avenir green fw4 f4">{queuer.party_size}</h4>
+    <div class="flex flex-row ph4 pv3 mv2 bg-white shadow-1">
+      <h4 class="index avenir black fw5 f4 w3">{index + 1}</h4>
+      <h4 class="name avenir black fw3 f4 w5">{queuer.name}</h4>
+      <h4 class="name avenir green fw4 f4 w3">{queuer.party_size}</h4>
+      <h4 class="avenir black fw4 f4"></h4>
     </div>
   ))
 )
 
 const view = (state, actions) => (
   <main
-    oncreate={() => actions.setQueuers()}
+    oncreate={() => {
+      actions.setQueuers()
+    }}
     class="vw-100 h-100 bg-light-gray"
   >
     <div class="outer flex flex-row">
       <Nav />
       {state.queuers !== null &&
-        <div class="queue list flex flex-column mw6 w-100">
+        <div class="queue list flex flex-column mw6 w-100 center pb4 pt4">
           {renderQueue(state.queuers)}
         </div>
       }
