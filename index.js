672500b1615c4ca6a15dbf65f07ce031a79f5696
cwahlfeldt <chriswahlfeldt@gmail.com>
2018-07-21 17:38:40 -0500

    some styling

---
+++ index.js
@@ -0,0 +1,1 @@
+// server will be here
--- src/index.js
+++ src/index.js
@@ -1,35 +1,33 @@
 import { h, app } from 'hyperapp'
-import { queuers } from './mockdata.yml'
+import { queuers } from './lib/mockdata.yml'
 import Nav from './components/Nav'
 import moment from 'moment'
 
 import 'tachyons-custom'
-import './style.css'
-import './vars.css'
+import './lib/style.css'
+import './lib/vars.css'
 
 const data = queuers
 
 const state = {
   queuers: null,
-  waitingTimes: [],
+  openModal: false,
 }
 
 const actions = {
   setQueuers: state => ({queuers: data}),
-  startTimers: (time, index) => (state, actions) => {
-    setInterval(actions.setTime(time, index), 1000)
-  },
+  toggleModal: state => ({openModal: !state.openModal}),
 }
 
 const renderQueue = (queuers) => (
   queuers.map((queuer, index) => (
     <div>
       {queuer.end === null &&
-        <div class="flex flex-row ph4 pv3 mv2 bg-white shadow-1">
+        <div class="flex flex-row ph4 mv2 bg-white shadow-1 br2 items-center">
           <h4 class="index avenir black fw5 f4 w3 counter"></h4>
           <h4 class="name avenir black fw3 f4 w5">{queuer.name}</h4>
-          <h4 class="name avenir green fw4 f4 w3">{queuer.party_size}</h4>
-          <h4 class="avenir black fw4 f4"></h4>
+          <h4 class="name avenir black fw4 f4 w3 ph2 pv3">{queuer.party_size}</h4>
+          <h4 class="avenir black fw4 f5 ph2 pv3">12min</h4>
         </div>
       }
     </div>
@@ -41,7 +39,7 @@ const view = (state, actions) => (
     oncreate={() => {
       actions.setQueuers()
     }}
-    class="vw-100 h-100 bg-light-gray"
+    class="vw-100 h-100 bg-light-gray pr5"
   >
     <div class="outer flex flex-row counter-reset">
       <Nav />
@@ -50,8 +48,9 @@ const view = (state, actions) => (
           {renderQueue(state.queuers)}
         </div>
       }
+      <button class="add-button dim link button-reset bg-green fixed bottom-0 right-0 br-100 white aktiv pa3 lh- ma4 f2 w3 dib shadow-1">&#43;</button>
     </div>
   </main>
 )
 
-app(state, actions, view, document.body)
+const main = app(state, actions, view, document.body)
