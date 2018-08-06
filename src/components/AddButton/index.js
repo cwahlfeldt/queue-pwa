da3339436ef33acc8dae9b46ad7866a278d77446
cwahlfeldt <chriswahlfeldt@gmail.com>
2018-08-05 19:46:36 -0500

    styling

--- src/components/AddButton/index.js
+++ src/components/AddButton/index.js
@@ -5,9 +5,9 @@ const AddButton = ({isOpen, toggleModal, onAddQueue}) => (
     onclick={() => toggleModal()}
     class={`
       ${isOpen ? 'rotate-45' : 'rotate-90'}
-      add-button dim link button-reset
+      add-button button-reset
       z-3 bg-green fixed bottom-0 right-0
-      br-100 white aktiv pa3 lh- ma4 f2 w3
+      br-100 white aktiv pa3 lh- ma4 f2 fw5 w3
       dib shadow-1
     `}
   >
