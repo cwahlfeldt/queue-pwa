9f732a3c1cebd75c83fec85a832d9201922282f8
waffles <chriswahlfeldt@gmail.com>
2019-05-12 23:31:14 -0500

    cleabned up button

--- src/components/AddButton/index.js
+++ src/components/AddButton/index.js
@@ -1,4 +1,23 @@
-import {h} from 'hyperapp';
+//
+// The add button component is broken into 2 components
+import { h } from 'hyperapp'
+import picostyle from 'picostyle'
+
+const style = picostyle(h)
+
+const X = style(`div`)({
+  'font-size': '59px',
+    'margin-top': `-10px`,
+  'margin-left': `1px`,
+  'line-height': `${ square }px`,
+})
+
+const square = 65
+export const Button = style(`button`)({
+  width: `${ square }px`,
+  height: `${ square }px`,
+  bottom: `15px`, right: `15px`,
+})
 
 const AddButton = ({
   isOpen,
@@ -6,21 +25,21 @@ const AddButton = ({
   goHome,
   id
 }) => (
-  <button
+  <Button
     id={id}
     onclick={() => {
       goHome()
       toggleModal()
     }}
     class={`
-      ${isOpen ? 'rotate-45' : 'rotate-90'}
+      ${isOpen ? 'rotate-45 bg-red' : 'bg-green rotate-90'}
       add-button button-reset
-      z-3 bg-green fixed bottom-0 right-0
-      br-100 white aktiv pa3 lh- ma4 f2 fw5 w3
-      dib shadow-1
+      z-3 fixed font-thin
+      br-100 white aktiv
+      shadow-1
     `}>
-    <span class="dib">&#43;</span>
-  </button>
+    <X>&#43;</X>
+  </Button>
 );
 
 export default AddButton;
--- src/routes/Settings/index.js
+++ src/routes/Settings/index.js
@@ -1,5 +1,7 @@
-import { h } from 'hyperapp'
-import picostyle from 'picostyle'
+/* fuckin weird sugestion */
+import { h } from 'hyperapp' // nolint
+console.log(h(`div`, {}, `default`))
+
 import Container from '../../components/Container'
 
 const Settings = ({
@@ -8,10 +10,14 @@ const Settings = ({
 }) => (
   <Container
     oncreate={() => {
+      console.log(actions)
+      console.log(state)
       paypal.Buttons().render('#paypal-subscribe')
     }}
   >
-    <p className="black-80 mt2 pb3 center lh-copy avenir i f5">Ipsum nostrum adipisci assumenda quisquam repellendus quo Consequuntur facere odit pariatur totam deserunt, doloribus. Vitae veritatis delectus quam nesciunt totam. Laudantium placeat aperiam ad neque fuga. Earum soluta praesentium odit</p>
+    <p className="black-80 mt2 pb3 center lh-copy avenir i f5">
+      Ipsum nostrum adipisci assumenda quisquam repellendus quo Consequuntur facere odit pariatur totam deserunt, doloribus. Vitae veritatis delectus quam nesciunt totam. Laudantium placeat aperiam ad neque fuga. Earum soluta praesentium odit
+    </p>
     <div id="paypal-subscribe"></div>
   </Container>
 )
